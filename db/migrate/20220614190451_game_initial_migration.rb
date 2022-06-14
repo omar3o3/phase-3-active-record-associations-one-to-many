70522f10816e058957ba08257193f65023514a8a
omar3o3 <otantawy53@gmail.com>
2022-06-14 15:25:03 -0400

    commit tree 74ccc15ea91823e74855628363f9526cbb44be83

---
+++ db/migrate/20220614190451_game_initial_migration.rb
@@ -0,0 +1,11 @@
+class GameInitialMigration < ActiveRecord::Migration[6.1]
+  def change
+    create_table :games do |t|
+      t.string :title
+      t.string :platform
+      t.string :genre
+      t.integer :price
+      t.timestamps
+    end
+  end
+end
